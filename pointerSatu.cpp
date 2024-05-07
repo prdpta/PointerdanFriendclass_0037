d101fe475967f077b96cfa142d7883294d130bf6
prdpta <pradiptaputramei01@gmail.com>
2024-05-07 17:38:18 +0700

    first commit

---
+++ pointerSatu.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa{
+    public:
+    int nim;
+    void showNim(){
+        cout<<"No Induk = "<<nim<<endl; 
+    }
+};
+
+int main (){
+
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); // Member Acces Operator
+
+    mahasiswa &refMhs = mhs; // Pointer Reference refMhs
+    refMhs.nim = 2; // Member Acces Operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; // Pointer Dereference pMhs
+    pMhs->nim = 3; // Arrow Operator
+    pMhs->showNim();
+    return 0;
+}
